ee49875c7fe45141d3d78e90fba9bb5a058894e9
Paul Kats <paulkats@data-zen.com>
2019-09-03 16:12:09 -0700

    SchoolMint Revesion1

---
+++ main.py
@@ -0,0 +1,254 @@
+from os import getenv
+import argparse
+import os
+import pandas as pd
+#from db import Connection
+from sqlsorcery_local import MSSQL
+from mailer import Mailer
+import logging
+import sys
+import traceback
+from data_config import custom_application_fields
+import downloadftp
+
+
+logging.basicConfig(
+	stream=sys.stdout,
+	level=logging.INFO,
+	format="%(asctime)s | %(levelname)s: %(message)s",
+	datefmt="%Y-%m-%d %I:%M:%S%p %Z",
+)
+
+
+
+def read_from_csv(CSVFilename):
+
+	#Ensure File Really Exists
+	if (not os.path.isfile(CSVFilename)):
+		raise Exception(f'Error: "{CSVFilename}" File Does Not Exists. Most likely problem downloading from sFTP')
+
+	#Load CSV into Pandas DF
+	df = pd.read_csv(CSVFilename,  sep=',', quotechar = '"' , doublequote = True,dtype=str,header=0)
+	df.rename(columns=custom_application_fields, inplace=True)
+
+	#Ensure Data is really in the DataFrame
+	RowsImported=(len(df.index))
+	if (RowsImported == 0):
+		raise Exception(f'Error - No Data Was Loaded From CSV File: "{CSVFilename}"')
+	else:
+		logging.info(f'{RowsImported} Rows Successfully Imported From CSV File')
+		
+	return RowsImported,df
+
+def insert_into_table(df,Schema,Table,prepare_sproc):
+	
+
+	conn = MSSQL()
+	result=conn.exec_sproc(prepare_sproc)
+	InitialRowCT=result.fetchone()[0]
+
+	#Ensure Destination Table is Clean and Ready to be Loaded
+	if (InitialRowCT == 0 ):
+
+		#Load table from DF
+		conn.insert_into(Table, df)
+
+		#Get Counts Of Loaded Data
+		sql=f"select count(1) ct from {Schema}.{Table};"
+		result=conn.query(sql)
+		RowCT=result['ct'].values[0]
+
+		
+		#Ensure data loaded successfully into destination table. If not, reload from backup table.
+		if (RowCT == 0 ):
+			sql=f"insert into {Schema}.{Table} select * from {Schema}.{Table}_backup;select count(1) ct from {Schema}.{Table};"
+			result=conn.query(sql)
+			InsertedRowCT=result['ct'].values[0]
+
+			raise Exception(f'Error No Rows Loaded Into Table. {InsertedRowCT} Rows Reverted From Backup Table')
+		else:
+			#Since we loaded data into destination table successfully. Lets get count of rows in final backup table.
+			sql=f"select count(1) ct from {Schema}.{Table}_backup;"
+			result=conn.query(sql)
+			BackupRowCT=result['ct'].values[0]
+			
+			logging.info(f'{BackupRowCT} Rows Successfully Loaded into Backup Table')
+			logging.info(f'{RowCT} Rows Successfully Loaded into Table')
+
+
+	else:
+		raise Exception('Error Loading Data. Table Was Not Truncated.')		
+
+	return BackupRowCT,RowCT
+
+
+def process_change_tracking():
+	##Generate Change History
+	conn = Connection()
+	if eval(getenv("DEV_DB_Environment", "False")):
+		#Development Environment
+		sproc='sproc_zdev_Paycom_Create_ChangeTracking_Entries'
+	else:
+		sproc='sproc_Paycom_Create_ChangeTracking_Entries'
+
+	result=conn.exec_sproc(sproc)	
+	ChangeTrackingInsertedRowCT=result.fetchone()[0]
+	logging.info(f'{ChangeTrackingInsertedRowCT} Rows Successfully Loaded into Change Log')
+	
+	return ChangeTrackingInsertedRowCT
+	#RowCT=conn.query(sql)
+
+
+def send_email_notifications():
+	conn = Connection()
+	if eval(getenv("DEV_DB_Environment", "False")):
+		#Development Environment
+		view='custom.vw_Paycom_EmailNotifications_GetUnsent_dev'	
+	else:
+		view='custom.vw_Paycom_EmailNotifications_GetUnsent'
+		
+	result=conn.query(f'select * from {view}')  ##View  
+	result.fillna("", inplace = True) 
+	EmailsSent=0
+	for index,row in result.iterrows():
+		Email_To=getenv("TEST_EMAIL_ADDRESS", row['Email_To'])
+		Email_CC=getenv("TEST_CC_ADDRESS", row['Email_CC'])
+		Email_BCC=getenv("TEST_BCC_ADDRESS",'')
+		EmailSubject_EventDate=row['EmailSubject_EventDate']
+		EmailSubject=f"{row['EmailBody_EventType']} - {row['Employee_Name']} ({row['EmailSubject_EventDate']} Change ID = {row['ChangeID']})"
+		msg=f'''Employee Change Event: {row['EmailBody_EventType']}
+Change Date in Paycom: {row['ChangeTracking_Date']}
+
+Actions Required: {row['EmailBody_Actions']}
+ 
+{row['EmailBody_Details_PlainText']}
+'''
+		mailer = Mailer()
+		mailer.send_mail( "EmployeeUpdates KIPP Bay Area <techprovisioning@kippbayarea.org>", msg, Email_CC, Email_BCC , Email_To, EmailSubject)
+		if eval(getenv("DEV_DB_Environment", "False")):
+			#Development Environment
+			sproc='sproc_Paycom_zdev_EmailNotifications_MarkSent'			
+		else:
+			sproc='sproc_Paycom_EmailNotifications_MarkSent'
+
+		result=conn.exec_sproc(f"{sproc} @ChangeID={row['ChangeID']}")
+		EmailsSent += 1
+
+	logging.info(f'{EmailsSent} Emails Sent')
+	return EmailsSent
+
+
+
+
+def process_email_notifications():
+	##Generate Email Notifications Rows
+	conn = Connection()
+	if eval(getenv("DEV_DB_Environment", "False")):
+		#Development Environment
+		sproc='sproc_zdev_Paycom_EmailNotifications_Populate'			
+	else:
+		sproc='sproc_Paycom_EmailNotifications_Populate'
+
+	result=conn.exec_sproc(sproc)
+	EmailNotificationsInsertedRowCT=result.fetchone()[0]
+	logging.info(f'{EmailNotificationsInsertedRowCT} Rows Successfully Loaded into Email Notifications')
+	
+	return EmailNotificationsInsertedRowCT
+
+
+def download_files(deletelocalfiles,sourcedir,localdir,finalCSVname,deleteRemoteFile):
+	#Clean Out Destination Directory
+	if deletelocalfiles:
+		filelist = [ filename for filename in os.listdir(localdir) ]
+		for filename in filelist:
+			if filename.endswith(".csv"):
+				os.remove(os.path.join(localdir, filename))
+
+	#DownloadNewFiles
+	# downloading = downloadftp.Connection()
+	# downloading.download_dir(sourcedir,localdir)
+	
+	#Rename Latest Downloaded File
+	# for filename in (sorted(os.listdir(localdir))):
+	# 	if filename.endswith(".csv"):
+	# 		src =localdir + '/' + filename 
+	# 		dst =localdir + '/' +  finalCSVname
+	# 		os.rename(src, dst) 
+
+	#Delete Files From Remote Server When Done Downloading
+	# if deleteRemoteFile:
+	# 	downloading.delete_files_remotedir(sourcedir)
+
+
+def main():
+	try:
+		#Set Up ENV Variables
+		Schema = getenv("DBSCHEMA", 'custom')
+
+		#eval is used here to convert value from string to boolean
+		deletelocalfiles= eval(getenv("DELETELOCALFILES", "True"))
+
+		if eval(getenv("DEV_DB_Environment", "False")):
+			#Development Environment
+			RawTable = getenv("DBRAWTABLE", 'schoolmint_zdevpk_ApplicationData_changehistory')
+			RawIndexTable = getenv("DBRAW_INDEX_TABLE", 'schoolmint_zdevpk_ApplicationDataIndex_raw')
+			raw_sproc='sproc_zdev_schoolmint_raw_preparetables'
+			index_sproc='sproc_zdev_schoolmint_rawindex_preparetables'
+		else:
+			raw_sproc='Prod Sproc'
+			index_sproc='Prod Sproc'
+
+
+		#Instantiate Mailer
+		mailer = Mailer()
+
+		#Download Latest Files
+		download_files(deletelocalfiles=deletelocalfiles,sourcedir='outbound',localdir='files',finalCSVname='AutomatedApplicationData2020.csv',deleteRemoteFile=eval(getenv("DELETE_REMOTE_FILE", "True")))
+
+		#Load Data Frame from Downloaded CSV
+		RawRowsImported, df= read_from_csv('files/AutomatedApplicationData2020.csv')
+
+		#Load Database from DataFrame
+		RawBackupRowCT, RawRowCT= insert_into_table(df, Schema,RawTable,raw_sproc)
+	
+		#Load Data Frame from Downloaded CSV
+		RawIndexRowsImported, df= read_from_csv('files/AutomatedApplicationDataIndex2020.csv')
+
+		#Load Database from DataFrame
+		RawIndexBackupRowCT, RawIndexRowCT= insert_into_table(df, Schema,RawIndexTable,index_sproc)
+
+		#Create Change Tracking Rows
+		# ChangeTrackingInsertedRowCT=process_change_tracking()
+
+		# #Create Email Notifications Rows
+		# EmailNotificationsInsertedRowCT=process_email_notifications()
+
+		# #Send Email Notifications
+		# EmailsSent=send_email_notifications()
+
+		#Send Success Message
+		msg=f'''
+		{RawRowsImported} Rows Successfully Imported From Raw CSV File
+		{RawBackupRowCT} Rows Successfully Loaded into Raw Backup Table
+		{RawRowCT} Rows Successfully Loaded into Raw Table
+		{RawIndexRowsImported} Rows Successfully Imported From Raw Index CSV File
+		{RawIndexRowCT} Rows Successfully Loaded into Raw Index Table
+		{RawIndexBackupRowCT} Rows Successfully Loaded into Raw Index Backup Table
+
+		'''
+		# {ChangeTrackingInsertedRowCT} Rows Successfully Loaded into Change Log
+		# {EmailNotificationsInsertedRowCT} Rows Successfully Loaded into Email Notifications Table
+		# {EmailsSent} Emails Sent
+		# '''
+		# mailer.notify(results=msg)
+
+
+	except Exception as e:
+		logging.exception(e)
+		stack_trace = traceback.format_exc()
+		mailer.notify(success=False, error_message=stack_trace)
+
+
+if __name__ == "__main__":
+	main()
+

d34dac52f58bccce8dc84c2619a4555ac77f84ba
Susan Xiong <susanxiong@KBAS-C1MWJ1FKJ1WK.local>
2019-09-20 16:54:40 -0700

    PROJ-374 Add SQL tables

---
+++ sql/tables/SchoolMint_FactDailyStatus.sql
@@ -0,0 +1,17 @@
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [custom].[SchoolMint_FactDailyStatus](
+	[ID] [int] NOT NULL,
+	[School] [varchar](500) NULL,
+	[SchoolID] [varchar](50) NULL,
+	[SchoolYear4DigitEnd] [varchar](4) NULL,
+	[GradeLevel] [varchar](500) NULL,
+	[StatusName] [varchar](128) NULL,
+	[CountInStatus] [int] NULL,
+	[ReportDate] [date] NOT NULL,
+	[Month_Boolean]  AS (case when datepart(day,[ReportDate])=(1) then (1) else (0) end),
+	[Sunday_Boolean]  AS (case when datepart(weekday,[ReportDate])=(1) then (1) else (0) end)
+) ON [PRIMARY]
+GO
---
+++ sql/tables/SchoolMint_SchoolCodes.sql
@@ -0,0 +1,10 @@
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [custom].[SchoolMint_SchoolCodes](
+	[School] [varchar](50) NOT NULL,
+	[SchoolID] [varchar](50) NOT NULL,
+	[SchoolMint_SchoolID] [varchar](50) NULL
+) ON [PRIMARY]
+GO

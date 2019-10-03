111fb562669aa6a11d35f619271a691d6d883da4
Susan Xiong <susanxiong@kbas-c1mwj1fkj1wk.attlocal.net>
2019-10-02 17:35:03 -0700

    PROJ-372 Check file date and refactor table insertion

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import argparse
-import datetime 
+from datetime import datetime
+import glob
 import logging
 import os
 from os import getenv
@@ -17,8 +18,6 @@ from mailer import Mailer
 
 LOCALDIR = 'files'
 SOURCEDIR = 'schoolmint'
-RAWTABLE = getenv("DB_RAW_TABLE", 'schoolmint_ApplicationData_raw')
-RAWINDEXTABLE = getenv("DB_RAW_INDEX_TABLE", 'schoolmint_ApplicationDataIndex_raw')
 
 
 logging.basicConfig(
@@ -33,64 +32,49 @@ def read_logs(filename):
 	with open(filename) as f:
 		return f.read()
 
-def read_from_csv(CSVFilename):
 
-	#Ensure File Really Exists
+def read_csv_to_df(CSVFilename):
 	if (not os.path.isfile(CSVFilename)):
-		raise Exception(f'Error: "{CSVFilename}" File Does Not Exists. Most likely problem downloading from sFTP')
-
-	#Load CSV into Pandas DF
-	df = pd.read_csv(CSVFilename,  sep=',', quotechar = '"' , doublequote = True,dtype=str,header=0)
-
-	#Ensure Data is really in the DataFrame
-	RowsImported=(len(df.index))
-	if (RowsImported == 0):
-		raise Exception(f'Error - No Data Was Loaded From CSV File: "{CSVFilename}"')
+		raise Exception(f"Error: '{CSVFilename}' File Does Not Exist. Most likely problem downloading from sFTP")
+	df = pd.read_csv(CSVFilename, sep=',', quotechar='"', doublequote=True, dtype=str, header=0)
+	if len(df.index)==0:
+		raise Exception(f"Error - No Data Was Loaded From CSV File: '{CSVFilename}'")
 	else:
-		logging.info(f'{RowsImported} Rows Successfully Imported From CSV File')
-		
-	return RowsImported,df
-
-def insert_into_table(df,Schema,Table,prepare_sproc):
-	
-
-	conn = MSSQL()
-	result=conn.exec_sproc(prepare_sproc)
-	InitialRowCT=result.fetchone()[0]
-
-	#Ensure Destination Table is Clean and Ready to be Loaded
-	if (InitialRowCT == 0 ):
-
-		#Load table from DF
-		conn.insert_into(Table, df)
-
-		#Get Counts Of Loaded Data
-		sql=f"select count(1) ct from {Schema}.{Table};"
-		result=conn.query(sql)
-		RowCT=result['ct'].values[0]
-
-		
-		#Ensure data loaded successfully into destination table. If not, reload from backup table.
-		if (RowCT == 0 ):
-			sql=f"insert into {Schema}.{Table} select * from {Schema}.{Table}_backup;select count(1) ct from {Schema}.{Table};"
-			result=conn.query(sql)
-			InsertedRowCT=result['ct'].values[0]
-
-			raise Exception(f'Error No Rows Loaded Into Table. {InsertedRowCT} Rows Reverted From Backup Table')
-		else:
-			#Since we loaded data into destination table successfully. Lets get count of rows in final backup table.
-			sql=f"select count(1) ct from {Schema}.{Table}_backup;"
-			result=conn.query(sql)
-			BackupRowCT=result['ct'].values[0]
-			
-			logging.info(f'{BackupRowCT} Rows Successfully Loaded into Backup Table')
-			logging.info(f'{RowCT} Rows Successfully Loaded into Table')
+		logging.info(f'{len(df.index)} Rows Successfully Imported From CSV File')
+	return df
 
 
+def backup_and_truncate_table(conn, prepare_sproc):
+	result = conn.exec_sproc(prepare_sproc)
+	InitialRowCT = result.fetchone()[0]
+	if InitialRowCT == 0:
+		return True
 	else:
 		raise Exception('Error Loading Data. Table Was Not Truncated.')		
 
-	return BackupRowCT,RowCT
+
+def get_table_row_count(conn, Schema, Table):
+	result = conn.query(f"select count(1) ct from {Schema}.{Table};")
+	count = result['ct'].values[0]
+	return count
+
+
+def load_from_backup_table(conn, Schema, Table):
+	sql=f"insert into {Schema}.{Table} select * from {Schema}.{Table}_backup;"
+	result=conn.query(sql)
+	InsertedRowCT = get_table_row_count(conn, Schema, Table)
+	raise Exception(f'Error No Rows Loaded Into Table. {InsertedRowCT} Rows Reverted From Backup Table')
+
+
+def check_table_load(conn, Schema, Table):
+	"""Ensure data loaded successfully into destination table. If not, reload from backup table."""
+	RowCT = get_table_row_count(conn, Schema, Table)
+	if (RowCT == 0):
+		load_from_backup_table(conn, Schema, Table)
+	else:
+		BackupRowCT = get_table_row_count(conn, Schema, f'{Table}_backup')
+		logging.info(f'{BackupRowCT} Rows Successfully Loaded into Backup Table {Table}_backup')
+		logging.info(f'{RowCT} Rows Successfully Loaded into Table {Table}')	
 
 
 def process_change_tracking():
@@ -135,18 +119,13 @@ def delete_data_files(directory):
 			os.remove(os.path.join(directory, filename))
 
 
-def rename_file(finalCSVname=None, stringmatch=""):
-	dst = ""
-	filelist = sorted(os.listdir(LOCALDIR), key=lambda x: os.path.getctime(LOCALDIR + '/' + x))   
-	for filename in filelist:
-	 	if stringmatch in filename:
-	 		src = f'{LOCALDIR}/{filename}'
-	 		dst = f'{LOCALDIR}/{finalCSVname}'
-	 		os.rename(src, dst)
-	if os.path.exists(dst):
-		logging.info(f"'{dst}' Successfully Downloaded")
+def check_todays_file_exists(filename):
+	date = datetime.now().strftime('%m%d%y')
+	expected_filename = f"{LOCALDIR}/{filename}*{date}*.csv"
+	if len(glob.glob(expected_filename))==0:
+		raise Exception(f"Error: '{filename}' was not downloaded.")
 	else:
-		raise Exception(f"Error: '{LOCALDIR}/{finalCSVname}' Was Not Successfully Downloaded")
+		logging.info(f"'{expected_filename}' successfully downloaded")
 
 
 #Try Every 30 Seconds for 30 minutes
@@ -155,39 +134,60 @@ def download_from_ftp():
 	# TODO: all files are getting downloaded to local since we aren't deleting remote files
 	conn = ftp.Connection()
 	conn.download_dir(SOURCEDIR,LOCALDIR)
+	check_todays_file_exists("Automated Application Data Raw")
+	check_todays_file_exists("Automated Application Data Index")
+
+
+def rename_file(finalCSVname=None, stringmatch=""):
+	dst = ""
+	filelist = sorted(os.listdir(LOCALDIR), key=lambda x: os.path.getctime(LOCALDIR + '/' + x))
+	for filename in filelist:
+	 	if stringmatch in filename:
+	 		src = f'{LOCALDIR}/{filename}'
+	 		dst = f'{LOCALDIR}/{finalCSVname}'
+	 		os.rename(src, dst)
+	if os.path.exists(dst):
+		logging.info(f"'{dst}' Successfully Renamed")
+	else:
+		raise Exception(f"Error: '{LOCALDIR}/{finalCSVname}' Was Not Successfully Renamed")
+	return dst
 
 
 def main():
 	try:
-		Schema = getenv("DBSCHEMA", 'custom')
-
-		raw_sproc='sproc_SchoolMint_Raw_PrepareTables'
-		index_sproc='sproc_SchoolMint_RawIndex_PrepareTables'
-
+		schema = getenv("DBSCHEMA")
+		conn = MSSQL(schema=schema)
 		mailer = Mailer()
 
+		# get files
 		api_request()
-
 		if eval(getenv("DELETE_LOCAL_FILES", "True")):
 			delete_data_files(LOCALDIR)
-
 		download_from_ftp()
-		rename_file(finalCSVname='AutomatedApplicationData2020.csv',stringmatch="Data Raw")
-		rename_file(finalCSVname='AutomatedApplicationDataIndex2020.csv',stringmatch="Data Index")
-		# RawRowsImported, df= read_from_csv('files/AutomatedApplicationData2020.csv')
-		# df['SchoolYear4Digit'] = getenv("SchoolYear4Digit", '2021')
-		# RawBackupRowCT, RawRowCT= insert_into_table(df, Schema,RAWTABLE,raw_sproc)
 
-		# RawIndexRowsImported, df= read_from_csv('files/AutomatedApplicationDataIndex2020.csv')
-		# df['SchoolYear4Digit'] = getenv("SchoolYear4Digit", '2021')
-		# RawIndexBackupRowCT, RawIndexRowCT= insert_into_table(df, Schema,RAWINDEXTABLE,index_sproc)
+		# process Application Data file
+		dst = rename_file(finalCSVname='AutomatedApplicationData2020.csv',stringmatch="Data Raw")
+		df = read_csv_to_df(dst)
+		if backup_and_truncate_table(conn, getenv("SPROC_RAW_PREP")):
+			table = getenv("DB_RAW_TABLE")
+			conn.insert_into(table, df)
+			check_table_load(conn, schema, table)
 
+		# process Application Data Index file
+		dst = rename_file(finalCSVname='AutomatedApplicationDataIndex2020.csv',stringmatch="Data Index")
+		df = read_csv_to_df(dst)
+		if backup_and_truncate_table(conn, getenv("SPROC_RAW_INDEX_PREP")):
+			table = getenv("DB_RAW_INDEX_TABLE")
+			conn.insert_into(table, df)
+			check_table_load(conn, schema, table)
+
+		# execute sprocs
 		# ChangeTrackingInsertedRowCT=process_change_tracking()
 		# ChangeFactDailyStatusInsertedRowCT=process_FactDailyStatus()
 
+		# notification
 		success_message = read_logs("app.log")
 		mailer.notify(results=success_message)
-
 	except Exception as e:
 		logging.exception(e)
 		stack_trace = traceback.format_exc()

dd93827b8d37db4eb242d31b66754317a0434409
Susan Xiong <susanxiong@KBAS-C1MWJ1FKJ1WK.local>
2019-12-12 15:34:30 -0800

    PROJ-878 Update docstrings for Sphinx

--- ftp.py
+++ ftp.py
@@ -7,7 +7,10 @@ import pysftp
 
 
 class FTP:
+    """Class for the local FTP connection."""
+
     def __init__(self):
+        """Initialize connection to the FTP server."""
         FTP_HOST = os.getenv("FTP_HOSTNAME")
         FTP_USER = os.getenv("FTP_USERNAME")
         FTP_PWD = os.getenv("FTP_PWD")
@@ -20,27 +23,46 @@ class FTP:
         )
 
     def download_dir(self, remotedir, localdir):
-        """ Download all files from the remote directory to the local directory
-
-        There should be 3 items in the remote server when both files are downloaded,
-        including the archive folder. """
+        """
+        Download all files from the remote directory to the local directory.
+        
+        :param remotedir: Path of the remote directory that we are getting files from (ie. the FTP directory)
+        :type remotedir: String
+        :param localdir: Path of the local directory that we are saving files to
+        :type localdir: String
+        """
+        # 3 is the number of files plus the archive directory
         if len(self.ftpsrv.listdir(remotepath=remotedir)) >= 3:
             time.sleep(30)  # wait to allow the files to finish uploading
             self.ftpsrv.get_d(remotedir, localdir, preserve_mtime=True)
 
     def _archive_file(self, file):
-        """ Place the file in an 'archive' folder within its directory """
+        """
+        Place the file in an 'archive' folder within its directory.
+
+        :param file: Path and name of the file that will be archived. 
+        :type file: String
+        """
         self.ftpsrv.rename(
             file, file.replace(self.remotedir, f"{self.remotedir}/archive")
         )
 
     def _do_nothing(self, file):
-        """ Used for files and unknown file types """
+        """
+        Used for files and unknown file types.
+
+        :param file: Path and name of the file that will be ignored. 
+        :type file: String
+        """
         pass
 
     def archive_remote_files(self, remotedir):
-        """ Archive all of the files in the specified remote directory. 
-        Do nothing for directories and unknown file types """
+        """
+        Archive all of the files in the specified remote directory. 
+        
+        :param remotedir: Path of the remote directory (ie. the FTP directory)
+        :type remotedir: String
+        """
         self.remotedir = remotedir
         self.ftpsrv.walktree(
             self.remotedir,
@@ -51,14 +73,24 @@ class FTP:
         )
 
     def _delete_old_file(self, file):
-        """ Find the age limit based on the env variable and delete files older than this limit """
+        """
+        Find the age limit based on the env variable and delete files older than this limit.
+
+        :param file: Path and name of the file
+        :type file: String
+        """
         stat = self.ftpsrv.stat(file)
         age_limit = dt.datetime.now() - dt.timedelta(self.ARCHIVE_MAX_DAYS)
         if stat.st_mtime < age_limit.timestamp():
             self.ftpsrv.remove(file)
 
     def delete_old_archive_files(self, remotedir):
-        """ Delete old files from the archive folder """
+        """
+        Delete old files from the archive folder.
+
+        :param remotedir: Path of the remote directory (ie. the FTP directory)
+        :type remotedir: String
+        """
         archivedir = f"{remotedir}/archive"
         self.ftpsrv.walktree(
             archivedir,
--- mailer.py
+++ mailer.py
@@ -4,7 +4,10 @@ from email.mime.text import MIMEText
 
 
 class Mailer:
+    """Class to send email notifications about the job status."""
+
     def __init__(self):
+        """Initialize connection to the mail server."""
         self.user = getenv("GMAIL_USER")
         self.password = getenv("GMAIL_PWD")
         self.server = SMTP_SSL("smtp.gmail.com", 465)
@@ -14,19 +17,34 @@ class Mailer:
         self.to_bcc = getenv("BCC_ADDRESS", "")
 
     def _subject_line(self):
-        """ Return success/error in subject based on job status """
+        """
+        Return success/error in subject based on job status.
+        
+        :return: Job name and success/error status
+        :rtype: String
+        """
         subject_type = "Success" if self.success else "Error"
         return f"SchoolMint - {subject_type}"
 
     def _body_text(self):
-        """ Generate body message based on job status """
+        """
+        Generate email body message based on job status.
+        
+        :return: Email body with results or error message
+        :rtype: String
+        """
         if self.success:
             return f"SchoolMint was successful:\n{self.results}"
         else:
             return f"SchoolMint encountered an error:\n{self.error_message}"
 
     def _message(self):
-        """ Build the message body """
+        """
+        Build the email message body.
+        
+        :return: Message text
+        :rtype: MIMEText object
+        """
         msg = MIMEText(self._body_text())
         msg["Subject"] = self._subject_line()
         msg["From"] = self.from_address
@@ -34,7 +52,16 @@ class Mailer:
         return msg.as_string()
 
     def notify(self, results=None, success=True, error_message=None):
-        """ Send the notification message """
+        """
+        Send the notification message.
+        
+        :param results: contains the logger messages
+        :type results: String
+        :param success: True if the job was successful, False if there was an error
+        :type success: Boolean
+        :param error_message: contains stack trace if there is one
+        :type error_message: String
+        """
         self.results = results
         self.success = success
         self.error_message = error_message

9e5dac1bd3e0a9da4aea51b27fd120fdd8611c5f
dhess <dan.hess@kippbayarea.org>
2020-03-29 09:37:15 -0700

    Update names to use quoted identifiers

--- sql/postgresql/tables/schoolmint_SchoolCodes.sql
+++ sql/postgresql/tables/schoolmint_SchoolCodes.sql
@@ -1,5 +1,5 @@
-CREATE TABLE custom.schoolmint_SchoolCodes(
-	School varchar(50) NOT NULL,
-	SchoolID varchar(50) NOT NULL,
-	SchoolMint_SchoolID varchar(50) NULL
+CREATE TABLE custom."schoolmint_SchoolCodes"(
+	"School" varchar(50) NOT NULL,
+	"SchoolID" varchar(50) NOT NULL,
+	"SchoolMint_SchoolID" varchar(50) NULL
 )
--- sql/postgresql/tables/schoolmint_lk_Enrollment.sql
+++ sql/postgresql/tables/schoolmint_lk_Enrollment.sql
@@ -1,5 +1,5 @@
-CREATE TABLE custom.schoolmint_lk_Enrollment(
-	Enrollment_Period_id int NULL,
-	SchoolYear4Digit_int int NULL,
-	Enrollment_Period_str varchar(10) NULL
+CREATE TABLE custom."schoolmint_lk_Enrollment"(
+	"Enrollment_Period_id" int NULL,
+	"SchoolYear4Digit_int" int NULL,
+	"Enrollment_Period_str" varchar(10) NULL
 )
